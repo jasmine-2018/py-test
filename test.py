674eb2dc3d517223e43f0ad60f78efc107845898
fayewang(王玉菊) <fayewang@tencent.com>
2018-08-19 16:55:38 +0800

    读文件

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ import datetime,time
 with open('a.txt', 'w+') as f:
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     f.write(now)
+
+with open('a.txt', 'r') as f:
     print f.read()
 
-    print 1
+print 1
 time.sleep(10)
 print 2
