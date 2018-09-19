35d573582d566dccf7e48cbbbc963889bdb6b76b
jasmine-2018 <37621258+jasmine-2018@users.noreply.github.com>
2018-09-19 14:48:00 +0800

    Update test.py
    env test

--- test.py
+++ test.py
@@ -10,5 +10,8 @@ with open('a.txt', 'r') as f:
     print f.read()
 
 print 1
-time.sleep(10)
+time.sleep(2)
 print 2
+
+print "ENVO is {}".format(os.getenv('ENVO', None))
+print "TRAVIS_BUILD_NUMBER is {}".format(os.getenv('TRAVIS_BUILD_NUMBER', None))
