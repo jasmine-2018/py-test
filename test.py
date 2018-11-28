2959c96f867168924c68bde9111039b13c4af430
jasmine-2018 <37621258+jasmine-2018@users.noreply.github.com>
2018-11-28 17:32:08 +0800

    hello

--- test.py
+++ test.py
@@ -2,18 +2,20 @@
 
 import datetime,time,os
 
-with open('a.txt', 'a+') as f:
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    f.write(now)
+# with open('a.txt', 'a+') as f:
+#     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#     f.write(now)
 
-with open('a.txt', 'r') as f:
-    print f.read()
+# with open('a.txt', 'r') as f:
+#     print f.read()
 
-print 1
-time.sleep(2)
-print 2
+# print 1
+# time.sleep(2)
+# print 2
 
-envo = os.getenv('ENVO', None)
-print "ENVO is {}".format(envo)
-print "ENVO == test-11 : {}".format(envo == 'test-11')
-print "TRAVIS_BUILD_NUMBER is {}".format(os.getenv('TRAVIS_BUILD_NUMBER', None))
+# envo = os.getenv('ENVO', None)
+# print "ENVO is {}".format(envo)
+# print "ENVO == test-11 : {}".format(envo == 'test-11')
+# print "TRAVIS_BUILD_NUMBER is {}".format(os.getenv('TRAVIS_BUILD_NUMBER', None))
+
+print "hello"
